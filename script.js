974d7254d115db5ac28234f1ccb5783515f42545
Mastershivam7 <shivamkushwah98930@gmail.com>
2026-02-18 20:40:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+// Typing Effect
+const texts = ["Java Developer", "Spring Boot Developer", "Backend Engineer"];
+let index = 0;
+let charIndex = 0;
+const typingElement = document.querySelector(".typing");
+
+function type() {
+  if (charIndex < texts[index].length) {
+    typingElement.textContent += texts[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 1500);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingElement.textContent = texts[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 60);
+  } else {
+    index = (index + 1) % texts.length;
+    setTimeout(type, 300);
+  }
+}
+
+type();
+
+// Scroll Reveal
+window.addEventListener("scroll", () => {
+  document.querySelectorAll(".reveal").forEach((el) => {
+    const top = el.getBoundingClientRect().top;
+    const height = window.innerHeight;
+    if (top < height - 100) {
+      el.classList.add("active");
+    }
+  });
+});
